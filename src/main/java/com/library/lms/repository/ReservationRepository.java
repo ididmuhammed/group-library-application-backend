256c897d4957d6a292534e550b57e1916cdfa5c3
tabitha mugide <tabithamugide31@gmail.com>
2026-09-09 15:31:23 +0300

    created reservation repo

--- src/main/java/com/library/lms/repository/ReservationRepository.java
+++ src/main/java/com/library/lms/repository/ReservationRepository.java
@@ -1,4 +1,26 @@
 package com.library.lms.repository;
 
-public class ReservationRepository {
+import com.library.lms.entity.Book;
+import com.library.lms.entity.Reservation;
+import com.library.lms.entity.Reservation.ReservationStatus;
+import com.library.lms.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByUser(User user);
+
+    List<Reservation> findByStatus(ReservationStatus status);
+
+    List<Reservation> findByStatusAndPickupDeadlineBefore(ReservationStatus status, LocalDate date);
+
+    Optional<Reservation> findFirstByBookAndStatusOrderByReservedDateAsc(Book book, ReservationStatus status);
+
+    boolean existsByBookAndUserAndStatusIn(Book book, User user, List<ReservationStatus> statuses);
+
+    void deleteByBook(Book book);
 }
